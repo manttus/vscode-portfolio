9a91d559a84e7e7648848dacb17c9e14c0ac032d
Manttus <rbasnyat666@gmail.com>
2024-03-18 23:39:19 +0545

    feat: create editor footer

--- app/components/layouts/main/components/footer.tsx
+++ app/components/layouts/main/components/footer.tsx
@@ -1,9 +1,11 @@
+import SpriteIcon from "@/app/components/common/icon/sprites_icon";
+
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between w-full h-5 bg-footer text-foreground text-[0.65rem]">
       <div className="flex items-center h-full cursor-pointer">
         <div className="h-full flex justify-center items-center w-8 bg-remote flex-grow">
-          !
+          <SpriteIcon name="remote" size={12} className={"fill-foreground"} />
         </div>
       </div>
       <div className="px-2 cursor-pointer">Go Live</div>

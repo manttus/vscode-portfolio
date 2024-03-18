685d0b57ef71b326bdfae5ed4d53973cfd599a02
Manttus <rbasnyat666@gmail.com>
2024-03-18 23:38:59 +0545

    refactor: update data & ui for activity bar

--- app/components/layouts/main/components/editor-body.tsx
+++ app/components/layouts/main/components/editor-body.tsx
@@ -1,65 +1,76 @@
+"use client";
+
+import SpriteIcon from "@/app/components/common/icon/sprites_icon";
 import Image from "next/image";
 import { ReactNode } from "react";
+import { v4 as uuidv4 } from "uuid";
+import ActivityTile from "./atomic/activity-tile";
+import active from "@/app/components/lib/active";
 
 export default function EditorBody({ children }: { children: ReactNode }) {
   const activity_top = [
     {
-      icon: "svg/files.svg",
-      alt: "Files",
-      active: true,
+      id: uuidv4(),
+      icon: "files",
+      alt: "Files Icon",
+      active: active("/"),
+      path: "/",
+      tooltip: "Dashboard",
     },
     {
-      icon: "svg/vector.svg",
-      alt: "Search",
-      active: true,
+      id: uuidv4(),
+      icon: "search",
+      alt: "Search Icon",
+      active: active("/search"),
+      path: "/search",
+      tooltip: "Search",
     },
     {
-      icon: "svg/github.svg",
-      alt: "Github",
-      active: false,
+      id: uuidv4(),
+      icon: "github",
+      alt: "Github Icon",
+      active: active("/github"),
+      path: "/github",
+      tooltip: "Github",
     },
   ];
 
   const activity_bottom = [
     {
-      icon: "svg/setting.svg",
-      alt: "Setting",
-      active: false,
+      id: uuidv4(),
+      icon: "setting",
+      alt: "Setting Icon",
+      active: active("/setting"),
+      path: "/setting",
+      tooltip: "Settings",
     },
   ];
 
   return (
     <>
       <main className="flex flex-grow">
-        <aside className="flex flex-col justify-between items-center bg-activity w-11 py-1">
-          <section className="w-full">
-            {activity_top.map((activ) => {
-              return (
-                <div className="flex w-full justify-center cursor-pointer py-[0.80rem] border-l border-transparent hover:border-foreground">
-                  <Image
-                    className="h-6 w-6"
-                    src={activ.icon}
-                    alt={activ.alt}
-                    width={0}
-                    height={0}
-                  />
-                </div>
-              );
-            })}
-          </section>
-          <section className="w-full">
-            {activity_bottom.map((activ) => (
-              <div className="flex w-full justify-center cursor-pointer py-3 border-l border-transparent hover:border-foreground">
-                <Image
-                  className="h-6 w-6"
-                  src={activ.icon}
-                  alt={activ.alt}
-                  width={0}
-                  height={0}
-                />
-              </div>
+        <aside className="flex flex-col justify-between items-center bg-activity w-11">
+          <div className="w-full">
+            {activity_top.map((activ) => (
+              <ActivityTile
+                key={activ.id}
+                status={activ.active}
+                name={activ.icon}
+                path={activ.path}
+                tooltip={activ.tooltip}
+              />
             ))}
-          </section>
+          </div>
+          <div className="w-full">
+            {activity_bottom.map((activ) => (
+              <ActivityTile
+                key={activ.id}
+                status={activ.active}
+                name={activ.icon}
+                tooltip={activ.tooltip}
+              />
+            ))}
+          </div>
         </aside>
         <section className="bg-background">{children}</section>
       </main>

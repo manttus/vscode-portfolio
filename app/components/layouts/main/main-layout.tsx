c826460b622612c273514ac79b8c70e1145006c4
Manttus <rbasnyat666@gmail.com>
2024-03-20 22:29:29 +0545

    refactor: update main layout with new folder structure

--- app/components/layouts/main/main-layout.tsx
+++ app/components/layouts/main/main-layout.tsx
@@ -2,15 +2,22 @@
 
 import { ReactNode } from "react";
 import StatusBar from "./components/status";
-import EditorBody from "./components/editor-body";
 import Footer from "./components/footer";
+import useTabStore from "@/app/feature/store/useTabsStore";
+import EditorBody from "./components/sidebar/sidepanel";
+import TabBar from "./components/tabs/tab-bar";
+import SidePanel from "./components/sidebar/side-panel";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
+  const tabStore = useTabStore();
   return (
     <>
       <div className="flex flex-col h-screen w-screen bg-background">
         <StatusBar />
-        <EditorBody>{children}</EditorBody>
+        <SidePanel>
+          {tabStore.tabs.length !== 0 && <TabBar />}
+          {children}
+        </SidePanel>
         <Footer />
       </div>
     </>

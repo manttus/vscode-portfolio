2e7d50d231e5b77e401381ec9489a8b42189815b
Manttus <rbasnyat666@gmail.com>
2024-03-20 22:27:14 +0545

    refactor: update tab manipulation functions

--- app/feature/store/useTabsStore.ts
+++ app/feature/store/useTabsStore.ts
@@ -1,20 +1,58 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface ITab {
   id: string;
   title: string;
+  name: string;
 }
 
 interface ITabState {
   tabs: ITab[];
-  current: number;
+  current: string;
 }
 
-interface ITabAction {}
+interface ITabAction {
+  remove: (id: string) => void;
+  open: (tab: ITab) => void;
+  active: (id: string) => void;
+}
 
-const useTabStore = create<ITabState & ITabAction>((set) => ({
-  tabs: [],
-  current: 1,
-}));
+const useTabStore = create<ITabState & ITabAction>()(
+  persist(
+    (set) => ({
+      tabs: [],
+      current: " ",
+      active: (id: string) =>
+        set(() => {
+          return { current: id };
+        }),
+      open: (tab: ITab) =>
+        set((state) => {
+          const previous = state.tabs.find((prev) => prev.id === tab.id);
+          if (previous) {
+            return { current: previous.id };
+          }
+          let current = [...state.tabs];
+          current = current.concat(tab);
+          return { tabs: current, current: tab.id };
+        }),
+      remove: (id: string) =>
+        set((state) => {
+          const filtered = state.tabs.filter((tab) => tab.id !== id);
+          const selected =
+            filtered.length > 0 ? filtered[filtered.length - 1].id : null;
+          return {
+            tabs: filtered,
+            current: selected ? selected : state.current,
+          };
+        }),
+    }),
+    {
+      name: "tabs-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
 
 export default useTabStore;

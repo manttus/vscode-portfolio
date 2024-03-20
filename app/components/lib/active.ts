a6a6b9fc23a43c0c026e351f9345fab03fdfaac1
Manttus <rbasnyat666@gmail.com>
2024-03-20 22:29:51 +0545

    fix: fix active tab logic

--- app/components/lib/active.ts
+++ app/components/lib/active.ts
@@ -1,3 +1,7 @@
-export default function active(pathname: string) {
-  return pathname === "/";
+import { Tab } from "@/app/enums/TabEnums";
+import useActivityStore from "@/app/feature/store/useActivityStore";
+
+export default function active(tab: Tab) {
+  const activityStore = useActivityStore();
+  return activityStore.tab === tab;
 }

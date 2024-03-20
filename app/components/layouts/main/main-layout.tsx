53508ae9f9dbf8179351acecd483667371f7b0dd
Manttus <rbasnyat666@gmail.com>
2024-03-20 22:40:48 +0545

    fix: remove faulty import

--- app/components/layouts/main/main-layout.tsx
+++ app/components/layouts/main/main-layout.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from "react";
 import StatusBar from "./components/status";
 import Footer from "./components/footer";
 import useTabStore from "@/app/feature/store/useTabsStore";
-import EditorBody from "./components/sidebar/sidepanel";
 import TabBar from "./components/tabs/tab-bar";
 import SidePanel from "./components/sidebar/side-panel";
 

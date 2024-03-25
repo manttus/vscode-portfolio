a0b6e186bb102fa1bdd0f03b6033e85c57070b78
Manttus <rbasnyat666@gmail.com>
2024-03-25 22:44:31 +0545

    feat: use folder structure get action & handle no tabs

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { getStruture } from "@/actions/structure";
+import SpriteIcon from "../components/common/icon/sprites_icon";
+import CodeBlock from "../components/ui/code-block";
+import useTabStore from "../feature/store/useTabsStore";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <main></main>;
+  const { active, tabs } = useTabStore();
+
+  async function getData() {
+    const data = await getStruture();
+    console.log("Structure", data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <main className="flex h-full">
+      {tabs.length > 0 && (
+        <>
+          <div className="flex-grow">
+            <CodeBlock text="0.65rem" showLine />
+          </div>
+          <div className="px-2 pt-4 cursor-pointer">
+            <CodeBlock text="0.18rem" />
+          </div>
+        </>
+      )}
+      {tabs.length === 0 && (
+        <div className="flex w-full flex-grow justify-center items-center">
+          <SpriteIcon name="vscode" size={200} className="fill-[#121212]" />
+        </div>
+      )}
+    </main>
+  );
 }

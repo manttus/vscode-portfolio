c1b73b412254c6764fb80334b22065b5c183c59b
Manttus <rbasnyat666@gmail.com>
2024-03-27 01:27:37 +0545

    refactor: create tree structure in schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,7 +1,25 @@
-import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTable, text, uuid, boolean, pgEnum } from "drizzle-orm/pg-core";
+
+export const typeEnum = pgEnum("type", ["folder", "file", "image"]);
 
 export const structure = pgTable("structure", {
-  id: uuid("id").defaultRandom(),
-  filename: text("filename"),
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name"),
+  type: typeEnum("type"),
   extension: text("extension"),
+  icon: text("icon").default(" "),
+  content: text("content").default(" "),
+  parent_id: uuid("parent_id"),
 });
+
+export const childrenRelation = relations(structure, ({ one, many }) => ({
+  parent: one(structure, {
+    references: [structure.id],
+    fields: [structure.parent_id],
+    relationName: "structures",
+  }),
+  children: many(structure, {
+    relationName: "structures",
+  }),
+}));

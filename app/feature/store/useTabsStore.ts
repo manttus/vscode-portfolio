9f06d303156a2a921416d851ccbbbe1556e2a3a2
Manttus <rbasnyat666@gmail.com>
2024-03-27 01:28:24 +0545

    refactor: update tab contents in current

--- app/feature/store/useTabsStore.ts
+++ app/feature/store/useTabsStore.ts
@@ -5,46 +5,68 @@ interface ITab {
   id: string;
   title: string;
   name: string;
+  extension: string;
+  content: string;
 }
 
 interface ITabState {
   tabs: ITab[];
-  current: string;
+  current: {
+    id: string | null;
+    content: string;
+  };
 }
 
 interface ITabAction {
   remove: (id: string) => void;
   open: (tab: ITab) => void;
-  active: (id: string) => void;
+  active: (id: string, content: string) => void;
 }
 
 const useTabStore = create<ITabState & ITabAction>()(
   persist(
     (set) => ({
       tabs: [],
-      current: " ",
-      active: (id: string) =>
+      current: {
+        id: null,
+        content: "",
+      },
+      active: (id: string, content: string) =>
         set(() => {
-          return { current: id };
+          return {
+            current: {
+              id,
+              content,
+            },
+          };
         }),
       open: (tab: ITab) =>
         set((state) => {
           const previous = state.tabs.find((prev) => prev.id === tab.id);
           if (previous) {
-            return { current: previous.id };
+            return { current: { id: previous.id, content: previous.content } };
           }
           let current = [...state.tabs];
           current = current.concat(tab);
-          return { tabs: current, current: tab.id };
+          return {
+            tabs: current,
+            current: {
+              id: tab.id,
+              content: tab.content,
+            },
+          };
         }),
       remove: (id: string) =>
         set((state) => {
           const filtered = state.tabs.filter((tab) => tab.id !== id);
           const selected =
-            filtered.length > 0 ? filtered[filtered.length - 1].id : null;
+            filtered.length > 0 ? filtered[filtered.length - 1] : null;
           return {
             tabs: filtered,
-            current: selected ? selected : state.current,
+            current: {
+              id: selected ? selected.id : state.current.id,
+              content: selected ? selected.content : state.current.content,
+            },
           };
         }),
     }),
